7e5f4d9f2f168ddacb8d2c2bfbcd60bd187ebc03
Matthias Riegler <matthias.riegler@ankorstore.com>
2023-11-16 11:28:48 +0100

    write data
    Signed-off-by: Matthias Riegler <matthias.riegler@ankorstore.com>

--- test.py
+++ test.py
@@ -2,15 +2,17 @@ from googleapiclient.discovery import build
 
 def main():
     service = build("sheets", "v4")
-    spreadsheet = {"properties": {"title": "Hello world"}}
+    spreadsheet = {"properties": {"title": "My beloved sheet"}}
     spreadsheet = (
         service.spreadsheets()
         .create(body=spreadsheet, fields="spreadsheetId")
         .execute()
     )
-    print(f"Spreadsheet ID: {(spreadsheet.get('spreadsheetId'))}")
-    return spreadsheet.get("spreadsheetId")
+    spreadsheet_id = spreadsheet.get("spreadsheetId")
+    body = {"valueInputOption": "USER_ENTERED", "data": [{"range": "A1:C2", "majorDimension": "ROWS", "values": [["Hello", "World", "!"], ["Python", "is", "Fun"]]}]}
 
+    res = service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheet_id, body=body).execute()
+    print(res)
 
 if __name__ == "__main__":
   main()
